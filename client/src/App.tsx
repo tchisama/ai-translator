7e2c702ba3bd3810095ce4d65be663874382b7d8
tchi-sama <tchisama41@gmail.com>
2023-09-20 08:43:35 -0700

    styling and adding the two markers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React,{useState} from 'react';
+import { GoogleMap, useJsApiLoader , Marker } from '@react-google-maps/api';
+import markerIcon from "./assets/marker.png"
+import userMarkerIcon from "./assets/userMarker.png"
 
 const containerStyle = {
   width: '100%',
@@ -7,9 +9,39 @@ const containerStyle = {
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 0,
+  lng: 0
 };
+
+const customMapStyle = [
+  {
+    featureType: 'all',
+    elementType: 'all',
+    stylers: [{ color: '#EC4754' }] // Set the color to white
+  },
+  {
+    featureType: 'administrative',
+    elementType: 'geometry.stroke', // Target borders
+    stylers: [{ color: '#FF959D' }] // Set the border color to white
+  },
+  {
+    featureType: 'water',
+    elementType: 'all',
+    stylers: [{ color: '#282A3C' }] // Set the color to blue
+  },
+  {
+    featureType: 'label',
+    elementType: 'labels.text.fill',
+    stylers: [{ color: '#000000' }] // Set the label text color to black
+  },
+];
+
+
+
+
+
+
+
 
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
@@ -17,7 +49,11 @@ function MyComponent() {
     googleMapsApiKey: "AIzaSyAveqpCFkz-lZ3LLiYwzzMgQJLJll-AghQ"
   });
 
-  const [map, setMap] = React.useState<google.maps.Map | null>(null); // Provide the appropriate type
+  const [map, setMap] = useState<google.maps.Map | null>(null); // Provide the appropriate type
+  const [markerPositions, setMarkerPositions] = useState([
+    { lat: 0, lng: 0 }, // Initial position for marker 1
+    { lat: 10, lng: 10 } // Initial position for marker 2
+  ]);
 
   const onLoad = React.useCallback(function callback(map: google.maps.Map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
@@ -31,19 +67,41 @@ function MyComponent() {
     setMap(null)
   }, [])
 
+
+  const handleMarkerDrag = (index: number, position: google.maps.LatLngLiteral) => {
+    const updatedPositions = [...markerPositions];
+    updatedPositions[index] = position;
+    setMarkerPositions(updatedPositions);
+  };
+
+
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
         center={center}
-        zoom={10}
+        zoom={3}
         onLoad={onLoad}
         onUnmount={onUnmount}
         options={{
-          disableDefaultUI: true // Remove default UI elements (zoom and fullscreen buttons)
+          disableDefaultUI: true, // Remove default UI elements (zoom and fullscreen buttons)
+          minZoom: 3, // Set the minimum allowed zoom level
+          maxZoom: 4, // Set the maximum allowed zoom level
+          styles: customMapStyle // Apply the custom map style
         }}
       >
         { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+        {markerPositions.map((position, index) => (
+        <Marker
+          key={index}
+          position={position}
+          draggable={true}
+          onDragEnd={(e) => handleMarkerDrag(index, (e.latLng as any).toJSON())}
+          icon={{
+            url:index==0?markerIcon:userMarkerIcon,
+            scaledSize: new window.google.maps.Size(40, 40),
+          }}
+        />
+      ))}
       </GoogleMap>
   ) : <></>
 }
